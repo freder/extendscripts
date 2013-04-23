eb355a127f2392c0c9341cdb78bf0247a484d289
freder <der.froedi@gmail.com>
2013-04-23 22:32:36 +0200

    built settings dialog

--- indesign/flattersatz/flattersatz.js
+++ indesign/flattersatz/flattersatz.js
@@ -1,4 +1,4 @@
-#include "../_common/helpers.js"
+﻿#include "../_common/helpers.js"
 
 (function() {
 	function init() {
@@ -16,6 +16,54 @@
 	var len = app.selection.length;
 	if (len === 0) {
 		alert("select a text frame first");
+		return;
+	}
+	
+	var flatterzone = 15; // %
+	var randomness = 0; // %
+	var w = new Window("dialog", "settings");
+		w.orientation = "row";
+		w.alignChildren = "top";
+
+		var main_group = w.add("group");
+		main_group.orientation = "row";
+
+			var settings_group = main_group.add("group");
+			settings_group.orientation = "column";
+
+				var flatterzone_group = settings_group.add("group");
+				flatterzone_group.orientation = "row";
+				flatterzone_group.alignment = "right";
+					flatterzone_group.add("statictext", undefined, "flatterzone:");
+					var e = flatterzone_group.add("statictext", undefined, flatterzone+"%");
+					e.characters = 4;
+					var slider = flatterzone_group.add("slider", undefined, flatterzone, 0, 100);
+					slider.onChanging = function() {
+						e.text = slider.value+"%";
+						flatterzone = parseInt(slider.value);
+					}
+
+				var randomness_group = settings_group.add("group");
+				randomness_group.orientation = "row";
+				randomness_group.alignment = "right";
+					randomness_group.add("statictext", undefined, "randomness:");
+					var e2 = randomness_group.add("statictext", undefined, randomness+"%");
+					e2.characters = 4;
+					var slider2 = randomness_group.add("slider", undefined, randomness, 0, 100);
+					slider2.onChanging = function() {
+						e2.text = slider2.value+"%";
+						randomness = parseInt(slider2.value);
+					}
+
+			var btn_group = main_group.add("group");
+			btn_group.orientation = "column";
+				btn_group.add("button", undefined, "OK");
+				btn_group.add("button", undefined, "Cancel");
+
+	if (w.show() == 1) {
+		// ok
+	} else {
+		// cancel
 		return;
 	}
 
